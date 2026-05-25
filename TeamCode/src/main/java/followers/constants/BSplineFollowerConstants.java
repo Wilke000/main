9d79c53cbc901118a9e585c82aaf2a0953cfb6c8
paradoxical-duck <sohumarora28@gmail.com>
2026-05-25 17:22:11 +0530

    Add BSplineFollowerConstants

---
+++ TeamCode/src/main/java/followers/constants/BSplineFollowerConstants.java
@@ -0,0 +1,11 @@
+package followers.constants;
+
+public class BSplineFollowerConstants {
+    public double translationP = 0.1;
+    public double headingP = 0.4;
+    public double velocityFF = 0.01;
+
+    public double headingTolerance = Math.toRadians(1.0);
+    public double distanceTolerance = 0.5;
+    public double tTolerance = 0.95;
+}
